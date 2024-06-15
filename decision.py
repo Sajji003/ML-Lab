d6098d03237c8d58a58459114a8b0877578bb580
Sajji003 <160243389+Sajji003@users.noreply.github.com>
2024-06-15 14:32:53 +0530

    Add files via upload

---
+++ decision.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=0)
+clf = DecisionTreeClassifier(max_leaf_nodes=10,
+random_state=0)
+clf.fit(X_train, y_train)
+tree.plot_tree(clf)
+plt.show()
+# print(clf.predict([[5.,3.,1.,.2]]))
+# print(clf.predict_proba([[5.,3.,1.,.2]]))
+predicted_data=clf.predict(X_test)
+print (clf.predict_proba(X_test))
+print(predicted_data)
+#print(y_test)
+cm=confusion_matrix(y_test,predicted_data)
+#print(cm)
+disp=ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.show()
